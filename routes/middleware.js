551d37387e4b8486381c9c46ae4cac6c8c6bb21e
Bill Hefty <billhefty@gmail.com>
2017-04-14 17:01:49 -0500

    cleanup unused routes

--- routes/middleware.js
+++ routes/middleware.js
@@ -23,7 +23,7 @@ exports.initLocals = function (req, res, next) {
 		{ label: 'Projects', key: 'projects', href: '#projects' },
 		{ label: 'Contact', key: 'contact', href: '#contact' },
 		// { label: 'Home', key: 'home', href: '/' },
-		// { label: 'Blog', key: 'blog', href: '/blog' },
+		{ label: 'Blog', key: 'blog', href: '/blog' },
 		// { label: 'Gallery', key: 'gallery', href: '/gallery' },
 		// { label: 'Contact', key: 'contact', href: '/contact' },
 	];
