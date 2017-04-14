aa0b598c392581d6e46a4d619b4a5ed03a6384f4
Bill Hefty <billhefty@gmail.com>
2017-04-14 13:01:26 -0500

    init commmit

---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+$('body').scrollspy({target: "#navigation", offset: 0})
+
+$('#navigation a, #cover a').on('click', function(event) {
+    if (this.hash !== '') {
+        event.preventDefault()
+
+        let hash = this.hash
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash
+        })
+    }
+})
+
+$('#navigation').affix({
+    offset: {
+        top: $('#cover').height()
+    }
+})
